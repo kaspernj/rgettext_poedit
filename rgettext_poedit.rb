e0ef3dcdd0d8c5a1928bd5c8ac6a258afc7e9466
Kasper Johansen <k@spernj.org>
2012-09-12 11:58:24 +0200

    Fixed new path.

--- rgettext_poedit.rb
+++ rgettext_poedit.rb
@@ -1,6 +1,4 @@
-#!/usr/bin/env ruby1.9.1
-
-require "knj/autoload"
+#!/usr/bin/env ruby1.9
 
 rgettext_argv = []
 skipnext = false
@@ -33,9 +31,7 @@ if !ofn
 end
 
 def debug(str)
-	if $debug
-		print str + "\n"
-	end
+	print "#{str}\n" if $debug
 end
 
 find_file = "gettext/tools/rgettext.rb"
@@ -51,30 +47,50 @@ end
 if !found_file
 	#Try looking in gems-folder.
 	require "rubygems"
-	Knj::Php.print_r(ENV)
+	found_file = nil
+	
 	Gem.path.each do |path|
-		file_path = "#{path}/gems/gettext-2.1.0/bin/rgettext"
-		if File.exists?(file_path)
-			found_file = file_path
-			break
-		end
+    Dir.foreach("#{path}/gems") do |file|
+      if match = file.match(/^gettext-.\..\..$/)
+        print "File: #{file}\n"
+        
+        file_path = "#{path}/gems/#{file}/bin/rxgettext"
+        print "Fpath: #{file_path}\n"
+        
+        if File.exists?(file_path)
+          print "File exists: #{file_path}"
+          found_file = file_path
+          break
+        end
+      end
+    end
+    
+    break if found_file
 	end
 	
 	if !found_file
-		print "Fatal error: File could not be found in $LOAD_PATH or Gem.path: #{find_file}.\n"
-		exit
+		print "Fatal error: File could not be found in $LOAD_PATH or Gem.path: '#{find_file}'.\n"
+		exit(-1)
 	end
 end
 
-rgettext_cmd = "ruby1.9.1 #{found_file} " + rgettext_argv.join(" ")
+rgettext_cmd = "ruby1.9.1 #{found_file} #{rgettext_argv.join(" ")}"
 debug "Command: " + rgettext_cmd
 
+add = true
 output = %x[#{rgettext_cmd}]
+
+if output.to_s.match(/Error parsing/)
+  debug "Dont add because 'error-parsing': #{found_file}"
+  add = false
+end
 
 debug "Output filename: " + ofn
 
-File.open(ofn, "a") do |file|
-	file.print(output)
+if add
+  File.open(ofn, "a") do |fp|
+    fp.print(output)
+  end
 end
 
-debug "Output:\n" + output
+debug "Output:\n#{output}"
