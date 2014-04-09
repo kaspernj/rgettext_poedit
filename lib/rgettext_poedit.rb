f00fd346b9f9274a72a11fc275e9f105c01a7ea6
Kasper Johansen <k@spernj.org>
2014-04-09 09:46:55 +0200

    Converted to Gem. Implemented much faster internal engine instead of rgettext which also supports Haml. Added tests for various cases.

---
+++ lib/rgettext_poedit.rb
@@ -0,0 +1,97 @@
+require "knjrbfw"
+
+class RgettextPoedit
+  def initialize(args)
+    @args  = args
+    @files = args[:files]
+    @translations = {}
+    @method_names = ["_", "gettext"]
+    @valid_beginning = '(^|\s+|\(|\{)'
+  end
+  
+  def parse_files
+    raise "No files was given." if !@files || @files.empty?
+    
+    @files.each do |filepath|
+      parse_filepath(filepath)
+    end
+  end
+  
+  def write_file
+    raise "No output-filepath was given." unless @args[:output_filepath]
+    
+    File.open(@args[:output_filepath], "w") do |fp|
+      fp.write(generate_output)
+    end
+  end
+  
+  def generate_output
+    # Generate and write output.
+    @output = ""
+    @translations.each do |translation, data|
+      @output << "\n" if @output.length > 0
+      
+      data[:files].each do |file|
+        @output << "#: #{file[:filepath]}:#{file[:line_no]}\n"
+      end
+      
+      @output << "msgid \"#{translation}\"\n"
+      @output << "msgstr \"\"\n"
+    end
+    
+    return @output
+  end
+  
+private
+  
+  # Opens a file, reads the content while keeping track of line-numbers and saves found translations.
+  def parse_filepath(filepath)
+    File.open(filepath, "r") do |fp|
+      line_no = 0
+      fp.each_line do |line|
+        line_no += 1
+        next if should_skip_line(filepath, line_no, line)
+        parse_content(filepath, line_no, line)
+      end
+    end
+  end
+  
+  # Scans content for translations and saves them.
+  def parse_content(filepath, line_no, content)
+    @method_names.each do |method_name|
+      # Scan for the various valid formats.
+      content.scan(/#{@valid_beginning}#{Regexp.escape(method_name)}\s*\("(.+?)"/) do |match|
+        add_translation(filepath, line_no, match[1])
+      end
+      
+      content.scan(/#{@valid_beginning}#{Regexp.escape(method_name)}\s*"(.+?)"/) do |match|
+        add_translation(filepath, line_no, match[1])
+      end
+      
+      content.scan(/#{@valid_beginning}#{Regexp.escape(method_name)}\s*\('(.+?)'/) do |match|
+        add_translation(filepath, line_no, match[1])
+      end
+      
+      content.scan(/#{@valid_beginning}#{Regexp.escape(method_name)}\s*'(.+?)'/) do |match|
+        add_translation(filepath, line_no, match[1])
+      end
+    end
+  end
+  
+  def should_skip_line(filepath, line_no, line)
+    # Skip the line if it is a comment in Haml.
+    if File.extname(filepath) == ".haml" && line.match(/^(\s*)-(\s*)#/)
+      return true
+    end
+    
+    return false
+  end
+  
+  def add_translation(filepath, line_no, translation)
+    if !@translations.key?(translation)
+      @translations[translation] = {:files => []}
+    end
+    
+    @translations[translation][:files] << {:filepath => filepath, :line_no => line_no}
+  end
+end
---
+++ spec/rgettext_poedit_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "RgettextPoedit" do
+  let(:rgp){ RgettextPoedit.new({}) }
+  
+  it "can parse various forms" do
+    lines = [
+      "  _('Test 1 %{name}', :name)  \n",
+      "  _ 'Test 2 %{name}', :name  \n",
+      "  _'Test 3 %{name}', :name  \n",
+      '  _("Test 4 %{name}", :name)  ',
+      '  _ "Test 5 %{name}", :name  ',
+      '  _"Test 6 %{name}", :name  ',
+      '  _"Test 7"  ',
+      "  _'Test 8'  \n",
+      "  str = \"Hejsa \#{_('Test 9')} "
+    ]
+    
+    lines.each do |line|
+      rgp.__send__(:parse_content, nil, nil, line)
+    end
+    
+    strs = rgp.instance_variable_get(:@translations)
+    
+    strs.keys.should include "Test 1 %{name}"
+    strs.keys.should include "Test 2 %{name}"
+    strs.keys.should include "Test 3 %{name}"
+    strs.keys.should include "Test 4 %{name}"
+    strs.keys.should include "Test 5 %{name}"
+    strs.keys.should include "Test 6 %{name}"
+    strs.keys.should include "Test 7"
+    strs.keys.should include "Test 8"
+    strs.keys.should include "Test 9"
+  end
+end
